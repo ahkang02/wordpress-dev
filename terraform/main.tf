418bcbc06a0582963144049ee1c26378589f67b8
Ah Kang <ahkang02@outlook.com>
2025-12-22 22:05:20 +0800

    fix: terraform format

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 module "network" {
   source = "./network"
-  
+
   vpc_cidr             = var.vpc_cidr
   public_subnet_cidrs  = var.public_subnet_cidrs
   private_subnet_cidrs = var.private_subnet_cidrs
@@ -9,13 +9,13 @@ module "network" {
 
 module "security" {
   source = "./security"
-  
+
   vpc_id = module.network.vpc_id
 }
 
 module "alb" {
   source = "./alb"
-  
+
   vpc_id          = module.network.vpc_id
   subnets         = module.network.public_subnet_ids
   security_groups = [module.security.alb_sg_id]
@@ -23,16 +23,16 @@ module "alb" {
 
 module "compute" {
   source = "./compute"
-  
-  instance_type     = var.instance_type
-  subnets           = module.network.private_subnet_ids
-  security_groups   = [module.security.ec2_sg_id]
-  target_group_arn  = module.alb.target_group_arn
+
+  instance_type    = var.instance_type
+  subnets          = module.network.private_subnet_ids
+  security_groups  = [module.security.ec2_sg_id]
+  target_group_arn = module.alb.target_group_arn
 }
 
 module "database" {
   source = "./database"
-  
+
   subnets         = module.network.private_subnet_ids
   security_groups = [module.security.rds_sg_id]
   instance_class  = var.db_instance_class
